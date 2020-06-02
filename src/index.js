1720e36ee3c1748a73744507aed92c78c7a18e53
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-01 19:55:19 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 const Home = () => {
   return (
-    <div>
-      <h1>Home!</h1>
-    </div>
+    <Router>
+      <Route path="/" component={Welcome} />
+    </Router>
   );
 };
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
+const Welcome = () => {
+  return <div>TEST</div>;
+};
+
+ReactDOM.render(<Home />, document.getElementById("root"));
